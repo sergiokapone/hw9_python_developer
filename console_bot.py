05ca1e9ccd1809dfb07cd0bf095371aff27afc6d
Sergiy <sergiy.ponomarenko@gmail.com>
2023-02-16 09:33:57 +0200

    Update console_bot.py

--- console_bot.py
+++ console_bot.py
@@ -8,7 +8,7 @@ def input_error(func):
         except KeyError:
             return f"{args[0]} does not appear in list"
         except ValueError:
-            return "Give me name and phone please"
+            return "Give me a name and phone please"
 
     return inner
 
@@ -32,15 +32,20 @@ def get_phone(*args):
     return contacts[args[0]]
 
 
-def undefined(*args):
-    return "What do you mean?"
-
-
 @input_error
 def add_contact(*args):
     if args[1]:
         contacts[args[0]] = args[1]
         return f"I add contact for <{args[0]}> with value <{args[1]}>"
+    else:
+        raise ValueError
+
+
+@input_error
+def change_contact(*args):
+    if contacts.get(args[0]):
+        contacts[args[0]] = args[1]
+        return f"I change contact for <{args[0]}> to value <{args[1]}>"
     else:
         raise ValueError
 
@@ -56,37 +61,33 @@ def get_handler(command, *ars):
     COMMANDS = {
         "hello": hello,
         "add": add_contact,
-        "change": add_contact,
+        "change": change_contact,
         "phone": get_phone,
         "show all": show_all,
         "good bye": good_bye,
         "close": good_bye,
         "exit": good_bye,
-        "": undefined,
     }
     return COMMANDS[command]
 
 
-def get_pattern():
+def main():
+
     pattern = re.compile(
         r"^[a-zA-Z\s,!?]*(hello|add|change|phone|show all|good bye|close|exit)\s*(\w*)\s*(\d*)"
     )
-    text = pattern.search(input(">>> "))
-    if text is None:
-        return ("", "", "")
-    return text.groups()
-
-
-def main():
 
     while True:
-        pattern = get_pattern()
-        handler = get_handler(pattern[0])
-        response = handler(pattern[1], pattern[2])
-        if response is False:
-            print("Good bye!")
-            break
-        print(response)
+        try:
+            text = pattern.search(input(">>> "))
+            handler = get_handler(*text.groups())
+            response = handler(text.group(2), text.group(3))
+            if response is False:
+                print("Good bye!")
+                break
+            print(response)
+        except AttributeError:
+            print("What do you mean?")
 
 
 if __name__ == "__main__":
